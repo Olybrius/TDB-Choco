d8e393061beee1a9c4bf1fe377a81239b39414ec
dams53 <damsplaystation@msn.com>
2014-05-30 09:12:02 +0200

    Test BDD qui marche

--- TDB-Choc/src/tdb/choco/ui/bug/ongletBugs.java
+++ TDB-Choc/src/tdb/choco/ui/bug/ongletBugs.java
@@ -1,37 +1,76 @@
 package tdb.choco.ui.bug;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
 
 public class ongletBugs extends CustomComponent {
 
+    String url = "jdbc:mysql://localhost/test";
+    String login = "root";
+    String passwrd = "";
+    Connection cn = null;
+    Statement st = null;
+    ResultSet rs = null;
+    List<String> nomBenchmarks = new ArrayList<String>();
+    List<String> nomProblemes = new ArrayList<String>();
+
     public ongletBugs() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            cn = DriverManager.getConnection(url, login, passwrd);
+
+            st = cn.createStatement();
+            String sql = "SELECT * FROM solvers";
+
+            rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                nomBenchmarks.add(rs.getString("NAME"));
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                cn.close();
+                st.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
-        /* Create the table with a caption. */
-        Table table = new Table("This is my Table");
 
-        /*
-         * Define the names and data types of columns. The "default value"
-         * parameter is meaningless here.
-         */
-        table.addContainerProperty("First Name", String.class, null);
-        table.addContainerProperty("Last Name", String.class, null);
-        table.addContainerProperty("Year", Integer.class, null);
+        Table table = new Table("Bugs");
 
-        /* Add a few items in the table. */
-        table.addItem(new Object[] { "Nicolaus", "Copernicus",
-                new Integer(1473) }, new Integer(1));
-        table.addItem(new Object[] { "Tycho", "Brahe", new Integer(1546) },
-                new Integer(2));
-        table.addItem(new Object[] { "Giordano", "Bruno", new Integer(1548) },
-                new Integer(3));
-        table.addItem(new Object[] { "Galileo", "Galilei", new Integer(1564) },
-                new Integer(4));
-        table.addItem(new Object[] { "Johannes", "Kepler", new Integer(1571) },
-                new Integer(5));
+        table.addContainerProperty("Type du bug", String.class, null);
+        table.addContainerProperty("Nom du Benchmark", String.class, null);
+        table.addContainerProperty("Nom du Problème", String.class, null);
+
+        table.addItem(new Object[] { "Solving_Time < 90000 et pas de solution",
+                "", "" }, new Integer(1));
+        for (int i = 2; i < 2 + nomBenchmarks.size(); i++) {
+            table.addItem(new Object[] { "", nomBenchmarks.get(i - 2), "" },
+                    new Integer(i));
+        }
+        table.addItem(new Object[] { "Valeur exceptionnelle", "", "" },
+                new Integer(1000));
+        table.addItem(new Object[] {
+                "Solving_Time < 90000 et pas la meilleure solution (MIN, MAX)",
+                "", "" }, new Integer(2000));
 
         layout.addComponent(table);
         setCompositionRoot(layout);
