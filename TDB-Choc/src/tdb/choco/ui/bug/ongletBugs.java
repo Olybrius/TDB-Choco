48621e5021a83907e3ededcef2b6c73271601641
dams53 <damsplaystation@msn.com>
2014-06-05 14:51:41 +0200

    Premier tableau marche (à voir pour le design)

--- TDB-Choc/src/tdb/choco/ui/bug/ongletBugs.java
+++ TDB-Choc/src/tdb/choco/ui/bug/ongletBugs.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.VerticalLayout;
 
 public class ongletBugs extends CustomComponent {
 
@@ -20,12 +20,13 @@ public class ongletBugs extends CustomComponent {
     Connection cn = null;
     Statement st = null;
     ResultSet rs = null;
-    List<String> nomBenchmarks1 = new ArrayList<String>();
-    List<String> nomProblemes1 = new ArrayList<String>();
+    List<String> nomProblemes1D = new ArrayList<String>();
+    List<String> nomProblemes1AD = new ArrayList<String>();
     List<String> nomBenchmarks2 = new ArrayList<String>();
     List<String> nomProblemes2 = new ArrayList<String>();
     List<String> nomBenchmarks3 = new ArrayList<String>();
     List<String> nomProblemes3 = new ArrayList<String>();
+    String dernierBenchmark, avantDernierBenchmark;
 
     public ongletBugs() {
 
@@ -35,21 +36,42 @@ public class ongletBugs extends CustomComponent {
             cn = DriverManager.getConnection(url, login, passwrd);
             st = cn.createStatement();
 
+            // Requête pour récupérer les deux derniers benchmarks
+            String bid = "SELECT MAX(bid) as bid FROM benchmarks";
+            rs = st.executeQuery(bid);
+            rs.next();
+            Integer dernierBID = rs.getInt("bid");
+            Integer avantDernierBID = dernierBID - 1;
+            bid = "SELECT name FROM benchmarks WHERE bid = " + avantDernierBID;
+            rs = st.executeQuery(bid);
+            rs.next();
+            avantDernierBenchmark = rs.getString("name");
+            bid = "SELECT name FROM benchmarks WHERE bid = " + dernierBID;
+            rs = st.executeQuery(bid);
+            rs.next();
+            dernierBenchmark = rs.getString("name");
+
             // Requête Solving_time < 900000 et pas de solution pour récupérer
-            // le nom du problème
-            String nomProbleme1 = "Select p.name FROM problems p, resolutions r WHERE r.solving_time<900000 AND r.nb_sol=0 AND r.pid=p.pid";
-            rs = st.executeQuery(nomProbleme1);
+            // le nom du problème avec le dernier benchmark
+            String nomProbleme1D = "Select p.name FROM problems p, resolutions r WHERE bid = "
+                    + dernierBID
+                    + " AND r.solving_time<900000 AND r.nb_sol=0 AND r.pid=p.pid";
+            rs = st.executeQuery(nomProbleme1D);
             while (rs.next()) {
-                nomProblemes1.add(rs.getString("name"));
+                nomProblemes1D.add(rs.getString("name"));
             }
 
             // Requête Solving_time < 900000 et pas de solution pour récupérer
-            // le nom du benchmark
-            String nomBenchmark1 = "Select b.name FROM benchmarks b, resolutions r WHERE r.solving_time<900000 AND r.nb_sol=0 AND r.bid=b.bid";
-            rs = st.executeQuery(nomBenchmark1);
+            // le nom du problème avec l'avant dernier benchmark
+            String nomProbleme1AD = "Select p.name FROM problems p, resolutions r WHERE bid = "
+                    + avantDernierBID
+                    + " AND r.solving_time<900000 AND r.nb_sol=0 AND r.pid=p.pid";
+            rs = st.executeQuery(nomProbleme1AD);
             while (rs.next()) {
-                nomBenchmarks1.add(rs.getString("name"));
+                nomProblemes1AD.add(rs.getString("name"));
             }
+
+            // *************AU DESSUS CA MARCHE !!!!!!****************
 
             // Requête valeur exceptionnelle pour récupérer le nom du problème
 
@@ -95,55 +117,37 @@ public class ongletBugs extends CustomComponent {
             }
         }
 
-        final HorizontalLayout layout = new HorizontalLayout();
+        final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
 
-        Table table1 = new Table("Bugs");
+        Table table = new Table("Bugs");
+        table.setSortEnabled(false);
+        table.addContainerProperty("Type du bug", String.class, null);
+        table.addContainerProperty("Nom du Benchmark", String.class, null);
+        table.addContainerProperty("Nom du Problème", String.class, null);
 
-        table1.addContainerProperty("Type du bug", String.class, null);
-        table1.addContainerProperty("Nom du Benchmark", String.class, null);
-        table1.addContainerProperty("Nom du Problème", String.class, null);
+        table.addItem(new Object[] { "Solving_Time < 90000 et pas de solution",
+                "", "" }, new Integer(0));
 
-        table1.addItem(new Object[] {
-                "Solving_Time < 90000 et pas de solution", "", "" },
-                new Integer(1));
-        for (int i = 2; i < 2 + nomProblemes1.size(); i++) {
-            table1.addItem(new Object[] { "", nomBenchmarks1.get(i - 2),
-                    nomProblemes1.get(i - 2) }, new Integer(i));
+        for (int i = 1; i < 1 + nomProblemes1AD.size(); i++) {
+            table.addItem(new Object[] { "", avantDernierBenchmark,
+                    nomProblemes1AD.get(i - 1) }, new Integer(i));
         }
 
-        layout.addComponent(table1);
+        for (int i = 1000; i < 1000 + nomProblemes1D.size(); i++) {
+            table.addItem(
+                    new Object[] { "", dernierBenchmark,
+                            nomProblemes1D.get(i - 1000) }, new Integer(i));
+        }
 
-        Table table2 = new Table("Bugs");
+        table.addItem(new Object[] { "Valeur exceptionnelle", "", "" },
+                new Integer(2000));
 
-        table2.addContainerProperty("Type du bug", String.class, null);
-        table2.addContainerProperty("Nom du Benchmark", String.class, null);
-        table2.addContainerProperty("Nom du Problème", String.class, null);
-
-        table2.addItem(new Object[] { "Valeur exceptionnelle", "", "" },
-                new Integer(1));
-        // for (int i = 2; i < 2 + nomProblemes2.size(); i++) {
-        // table1.addItem(new Object[] { "", nomBenchmarks2.get(i - 2),
-        // nomProblemes2.get(i - 2) }, new Integer(i));
-        // }
-
-        layout.addComponent(table2);
-
-        Table table3 = new Table("Bugs");
-
-        table3.addContainerProperty("Type du bug", String.class, null);
-        table3.addContainerProperty("Nom du Benchmark", String.class, null);
-        table3.addContainerProperty("Nom du Problème", String.class, null);
-
-        table3.addItem(new Object[] {
+        table.addItem(new Object[] {
                 "Solving_Time < 90000 et pas la meilleure solution (MIN, MAX)",
-                "", "" }, new Integer(1));
-        for (int i = 2; i < 2 + nomProblemes3.size(); i++) {
-            table1.addItem(new Object[] { "", nomBenchmarks3.get(i - 2),
-                    nomProblemes3.get(i - 2) }, new Integer(i));
-        }
+                "", "" }, new Integer(3000));
 
-        layout.addComponent(table3);
+        layout.addComponent(table);
 
         setCompositionRoot(layout);
     }
